141c33e173f6c752470b0bc697b0ef99759c3626
lqchy <lachlantheslayer@gmail.com>
2022-11-17 18:33:29 +0000

    Add ClientChatReceivedEvent for chat processing

--- src/main/java/com/cecer1/projects/mc/cecermclib/forge/environment/mod/ForgeProxy.java
+++ src/main/java/com/cecer1/projects/mc/cecermclib/forge/environment/mod/ForgeProxy.java
@@ -1,8 +1,14 @@
 package com.cecer1.projects.mc.cecermclib.forge.environment.mod;
 
 import com.cecer1.projects.mc.cecermclib.common.CecerMCLib;
+import com.cecer1.projects.mc.cecermclib.common.modules.chatmetadata.ChatMessageData;
+import com.cecer1.projects.mc.cecermclib.common.modules.chatmetadata.ChatMetadataModule;
 import com.cecer1.projects.mc.cecermclib.forge.environment.ForgeClientEnvironment;
 import com.cecer1.projects.mc.cecermclib.forge.environment.ForgeServerEnvironment;
+import com.cecer1.projects.mc.cecermclib.forge.modules.text.WrappedForgeComponent;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public abstract class ForgeProxy {
     public abstract void initEnvironment();
@@ -12,6 +18,13 @@ public abstract class ForgeProxy {
         @Override
         public void initEnvironment() {
             CecerMCLib.initEnvironment(new ForgeClientEnvironment());
+            MinecraftForge.EVENT_BUS.register(this);
+        }
+
+        @SubscribeEvent
+        public void onClientChatReceived(ClientChatReceivedEvent event) {
+            WrappedForgeComponent component = new WrappedForgeComponent(event.message);
+            CecerMCLib.get(ChatMetadataModule.class).process(new ChatMessageData(component, 0));
         }
     }
 
